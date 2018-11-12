cd125b6531c233bcaa37f43ee0f2de2c13c1b070
asankhaperera <asankha.perera@gmail.com>
2018-11-12 21:21:59 +0530

    first

--- RestaurantMenuDemo/lambda.js
+++ RestaurantMenuDemo/lambda.js
@@ -1,4 +1,19 @@
-exports.handler = function(event, context, callback) {
-    console.log("Hello APIX");
-    callback(null, {"message": "Successfully executed"});
+let AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = function (event, context, callback) {
+
+    ddb.put({
+        TableName: 'MenuItems',
+        Item: { 'itemCode': event.itemCode, 'itemName': item.name, 'itemPrice': event.itemPrice, 'itemType': event.itemType }
+    }).promise().then(function (data) {
+        //your logic goes here
+        callback(null, 'Sucessfully persisted');
+    }).catch(function (err) {
+        //handle error
+    });
+
+
+    console.log("Hello APIX2");
+    callback(null, { "message": "Successfully executed" });
 }
